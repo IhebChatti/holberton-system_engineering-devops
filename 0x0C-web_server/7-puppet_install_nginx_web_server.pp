a85c7b1f96b97cb6ad3ede83142eb90c2c31c516
IhebChatti <iheb.chatti@holbertonschool.com>
2020-08-05 15:35:14 +0100

    puppet install nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,19 +5,19 @@ package { 'nginx':
   ensure => 'installed',
 }
 
-exec { 'Holberton school':
+exec { 'HolbertonSchool':
   command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
   provider => 'shell',
+}
+
+file_line { '301 Moved Permanently':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=0MW0mDZysxc permanent;',
 }
 
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
-}
-
-file_line { '301 Moved Permanently'
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'server_name _;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=0MW0mDZysxc permanent;',
 }
