bab5dcf27ecb48b7a37ec02187e618700864e1f0
IhebChatti <iheb.chatti@holbertonschool.com>
2020-08-14 16:53:45 +0100

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,30 +3,30 @@
 exec { 'update':
   command  => '/usr/bin/apt-get update',
 }
-package { 'nginx':
+-> package { 'nginx':
   ensure  => 'installed',
   require => Exec['update'],
 }
 
-file { '/var/www/html/index.html':
+-> file { '/var/www/html/index.html':
   content => 'Holberton School',
 }
 
-file_line { '301 Moved Permanently':
+-> file_line { '301 Moved Permanently':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=0MW0mDZysxc permanent;',
 }
 
-file_line { 'X-Served-By':
+-> file_line { 'X-Served-By':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'add_header X-Served-By ${HOSTNAME};',
 }
 
-service { 'nginx':
+-> service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
