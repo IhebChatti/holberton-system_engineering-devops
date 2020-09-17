ddd05392a3eab930b6dedddb3d46f30365258cfb
IhebChatti <iheb.chatti@holbertonschool.com>
2020-09-17 15:11:02 +0100

    top 10 subreddit posts titles

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""[function that queries the Reddit API and prints the titles of
+    the first 10 hot posts listed for a given subreddit]
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """[get top ten posts from subreddit]
+
+    Args:
+        subreddit ([str]): [subreddit name]
+    """
+    url = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
+    headers = {"User-Agent": "Lothric_21"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 404:
+        print('None')
+        return
+    top_subs = response.json().get('data')
+    for sub in top_subs.get('children'):
+        print(sub.get('data').get('title'))
