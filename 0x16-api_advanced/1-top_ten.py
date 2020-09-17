0ce087719a7e0e921f913bc609b1bb3603b30403
IhebChatti <iheb.chatti@holbertonschool.com>
2020-09-17 15:23:55 +0100

    fix top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,12 +11,12 @@ def top_ten(subreddit):
     Args:
         subreddit ([str]): [subreddit name]
     """
-    url = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {"User-Agent": "Lothric_21"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         print('None')
         return
-    top_subs = response.json().get('data')
-    for sub in top_subs.get('children'):
+    hot_posts = response.json().get('data')
+    for sub in hot_posts.get('children'):
         print(sub.get('data').get('title'))
