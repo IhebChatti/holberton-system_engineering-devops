986776893e1c96243cb9d86c3b7fae87d640d0f6
IhebChatti <iheb.chatti@holbertonschool.com>
2020-10-08 14:38:02 +0100

    holberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,10 @@
-exec { 'fix limits.conf for holberton user':
+# puppet manifest to edit /etc/security limits.conf
+exec { 'fix hard nofile limit':
   command => 'sed -i \'s/^holberton hard nofile.*/holberton hard nofile 64000/g\' /etc/security/limits.conf',
   path    => '/bin/',
 }
 
-exec { 'fix limits.conf for holberton user':
+exec { 'fix soft nofile limit':
   command => 'sed -i \'s/^holberton soft nofile.*/holberton soft nofile 64000/g\' /etc/security/limits.conf',
   path    => '/bin/',
 }
